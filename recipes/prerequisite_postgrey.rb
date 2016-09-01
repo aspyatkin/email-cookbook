7817cf6f0d694392ecc5e1c995f184debd0c0dc6
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-09-01 09:27:46 +0400

    Configure whitelist_clients and whitelist_recipients options for postgrey

--- recipes/prerequisite_postgrey.rb
+++ recipes/prerequisite_postgrey.rb
@@ -12,6 +12,34 @@ service 'postgrey' do
   action [:enable, :start]
 end
 
+whitelist_recipients_conf = '/etc/postgrey/whitelist_recipients'
+
+template whitelist_recipients_conf do
+  source 'postgrey/whitelist_recipients.erb'
+  mode 0644
+  owner 'root'
+  group node['root_group']
+  variables(
+    whitelist_recipients: node[id]['postgrey'].fetch('whitelist_recipients', [])
+  )
+  action :create
+  notifies :restart, 'service[postgrey]', :delayed
+end
+
+whitelist_clients_conf = '/etc/postgrey/whitelist_clients'
+
+template whitelist_clients_conf do
+  source 'postgrey/whitelist_clients.erb'
+  mode 0644
+  owner 'root'
+  group node['root_group']
+  variables(
+    whitelist_clients: node[id]['postgrey'].fetch('whitelist_clients', [])
+  )
+  action :create
+  notifies :restart, 'service[postgrey]', :delayed
+end
+
 template '/etc/default/postgrey' do
   source 'postgrey/default.erb'
   mode 0644
@@ -19,7 +47,9 @@ template '/etc/default/postgrey' do
   group node['root_group']
   variables(
     host: node[id]['postgrey']['host'],
-    port: node[id]['postgrey']['port']
+    port: node[id]['postgrey']['port'],
+    whitelist_clients: whitelist_clients_conf,
+    whitelist_recipients: whitelist_recipients_conf
   )
   action :create
   notifies :restart, 'service[postgrey]', :delayed
