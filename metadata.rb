69e65c29fe9218b34f5a4b3c64a665a5d7e0bd2e
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2017-06-02 13:10:48 +0400

    Fix doveadm commands

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'email'
 maintainer 'Alexander Pyatkin'
 maintainer_email 'aspyatkin@gmail.com'
 license 'MIT'
-version '1.4.2'
+version '1.4.3'
 description 'Installs and configures email server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
