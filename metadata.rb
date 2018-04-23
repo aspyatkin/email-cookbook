637a0475cb095dcd5047d1ae16eb2904248c9acf
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2018-04-23 11:35:22 +0400

    Fix strict poise-python version dependency

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'email'
 maintainer 'Alexander Pyatkin'
 maintainer_email 'aspyatkin@gmail.com'
 license 'MIT'
-version '1.4.6'
+version '1.4.7'
 description 'Installs and configures email server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
@@ -16,7 +16,7 @@ depends 'composer', '~> 2.2.0'
 
 depends 'chef_nginx', '~> 6.0.1'
 depends 'tls', '~> 3.0.0'
-depends 'poise-python', '~> 1.6.0'
+depends 'poise-python', '>= 1.6.0'
 
 depends 'localdns', '~> 1.1.0'
 
