f6237aa95519912c7d11fae42952b2f08cdf8cf1
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2018-04-28 11:40:37 +0400

    Update dependencies

--- metadata.rb
+++ metadata.rb
@@ -2,19 +2,19 @@ name 'email'
 maintainer 'Alexander Pyatkin'
 maintainer_email 'aspyatkin@gmail.com'
 license 'MIT'
-version '1.4.7'
+version '1.5.0'
 description 'Installs and configures email server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 recipe 'email', 'Installs and configures email server'
 
-depends 'postgresql', '~> 4.0.6'
+depends 'postgresql', '~> 6.1.1'
 depends 'database', '~> 6.1.1'
 depends 'php', '~> 2.0.0'
-depends 'ark', '~> 1.1.0'
+depends 'ark', '>= 3.1.0'
 depends 'composer', '~> 2.2.0'
 
-depends 'chef_nginx', '~> 6.0.1'
+depends 'nginx'
 depends 'tls', '~> 3.0.0'
 depends 'poise-python', '>= 1.6.0'
 
